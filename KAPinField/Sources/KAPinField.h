080b992ef12b0f7b12cabad60be8477fe6029a0c
Basem Emara <contact@basememara.com>
2019-04-16 17:29:40 -0400

    Add Carthage support

---
+++ KAPinField/Sources/KAPinField.h
@@ -0,0 +1,19 @@
+//
+//  KAPinField.h
+//  KAPinField
+//
+//  Created by Basem Emara on 2019-04-16.
+//  Copyright © 2019 alexiscreuzot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KAPinField.
+FOUNDATION_EXPORT double KAPinFieldVersionNumber;
+
+//! Project version string for KAPinField.
+FOUNDATION_EXPORT const unsigned char KAPinFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KAPinField/PublicHeader.h>
+
+
